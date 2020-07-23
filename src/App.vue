117978f6090dd3762a5b1a12dadc31939f9b66c8
Brad Holland <hollanbm@gmail.com>
2020-07-22 23:14:36 -0400

    move data out of html and into script from App.vue - iterate over data

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
   <div id="app" class="small-container">
     <h1>Employees</h1>
 
-    <employee-table />
+    <employee-table v-bind:employees="employees" />
   </div>
 </template>
 
@@ -17,6 +17,27 @@ export default {
   name: 'App',
   components: {
     EmployeeTable
+  },
+  data () {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com'
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com'
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com'
+        }
+      ]
+    }
   }
 }
 </script>

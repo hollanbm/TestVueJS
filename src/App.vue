fdd758a98f152dc82dfaafc1dcb370165250be10
Brad Holland <hollanbm@gmail.com>
2020-07-22 22:52:29 -0400

    remove default <app> template use EmployeeTable template

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+  </div> -->
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+
+    <employee-table />
   </div>
 </template>
 
 <script>
+import EmployeeTable from '@/components/EmployeeTable.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    EmployeeTable
+  }
 }
 </script>
 
@@ -20,4 +30,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
+
 </style>
